9cdbbf4e61e3127a142052c71eab5e1d6c165134
kout95 <antoine.bernier@outlook.fr>
2022-02-14 17:26:04 +0100

    Commence l'intégration de la maquette

---
+++ src/context/mes-aides.org/components/breadcrumb.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="caidf-breadcrumb-wrapper">
+    <div class="caidf-breadcrumb">
+      <template v-for="(chapter, index) in chapters" :key="chapter.label">
+        <span v-if="index" class="caidf-breadcrumb-arrow"
+          ><Arrow-right></Arrow-right
+        ></span>
+        <router-link
+          :to="chapter.root"
+          class="caidf-breadcrumb-link"
+          :class="{
+            active: chapter.current,
+            done: chapter.done,
+            disable: !chapter.done,
+          }"
+          :tabindex="disabledLink(chapter, index) ? -1 : 0"
+        >
+          <Check class="caidf-check"></Check>
+          <span>{{ chapter.label }}</span>
+        </router-link>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+import ArrowRight from "@/context/mes-aides.org/components/arrow-right"
+import Check from "@/context/mes-aides.org/components/check"
+export default {
+  name: "Breadcrumb",
+  components: { Check, ArrowRight },
+  computed: {
+    chapters() {
+      return this.$state.chapters(
+        this.$route.path,
+        this.$store.getters.getAllSteps,
+        this.$store.getters.lastUnansweredStep?.path
+      )
+    },
+    isRecapitulatif() {
+      return this.$route.name === "recapitulatif"
+    },
+    isResultsPage() {
+      return (
+        this.$route.name === "resultats" ||
+        this.$route.name === "resultatsDetails"
+      )
+    },
+  },
+  methods: {
+    disabledLink(chapter, index) {
+      return index === 0
+        ? false
+        : !chapter.done && !this.chapters[index - 1].done
+    },
+    goBack() {
+      window?.history.back()
+    },
+  },
+}
+</script>
+
+<style type="text/css"></style>
