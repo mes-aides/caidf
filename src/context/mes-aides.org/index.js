03d6edbcee0055c7577311cc50fb6c5bb6d1bfcf
kout95 <antoine.bernier@outlook.fr>
2022-02-16 19:04:10 +0100

    Fait une première passe sur la page résultat

--- src/context/mes-aides.org/index.js
+++ src/context/mes-aides.org/index.js
@@ -2,12 +2,32 @@ import Home from "./home.vue"
 import BaseLayout from "./base-layout.vue"
 import Simulation from "./views/simulation.vue"
 
+const NAME = "mes-aides.org"
 export default {
   Home,
   BaseLayout,
   Simulation,
-  name: "mes-aides.org",
+  name: NAME,
   routes: [
+    {
+      path: "/simulation/resultats",
+      name: "resultat",
+      meta: {
+        headTitle: `Les résultats de ma simulation sur le simulateur d'aides ${NAME}`,
+      },
+      component: () =>
+        import(
+          /* webpackChunkName: "static" */ "@/context/mes-aides.org/views/resultats/resultats.vue"
+        ),
+    },
+    // {
+    //   name: "resultatsDetails",
+    //   path: "resultats/:droitId",
+    //   component: () =>
+    //     import(
+    //       /* webpackChunkName: "resultats" */ "./views/simulation/resultats-detail.vue"
+    //     ),
+    // },
     {
       path: "/a-propos",
       name: "a-propos",
