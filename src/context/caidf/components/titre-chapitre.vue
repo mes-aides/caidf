cff64697ac0af4f757db52672e024a78300a1e9b
kout95 <antoine.bernier@outlook.fr>
2022-02-22 11:58:04 +0100

    Ajoute le bouton récapitulatif et gère les boutons radio et checkbox

---
+++ src/context/caidf/components/titre-chapitre.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="aj-category-title-wrapper">
+    <div class="aj-category-title">
+      <h1>{{ title }}</h1>
+      <router-link
+        v-if="showMenuButton"
+        :to="{ name: 'recapitulatif' }"
+        class="caidf-recapitulatif-button"
+      >
+        <img src="/caidf/img/ic_document.svg" alt="Bouton récapitulatif" />
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import Chapters from "@/lib/chapters"
+
+export default {
+  name: "TitreChapitre",
+  computed: {
+    title() {
+      return this.getTitleByRoute(this.$route)
+    },
+    showMenuButton() {
+      return this.$route.name !== "recapitulatif"
+    },
+  },
+  methods: {
+    getTitleByRoute(route) {
+      const path = route.path
+      if (path === "/simulation/recapitulatif") {
+        return "Récapitulatif"
+      }
+
+      const current = path.replace(/\/en_savoir_plus/, "")
+      const step =
+        this.$store.getters.passSanityCheck &&
+        this.$state.current(current, this.$store.getters.getAllSteps)
+      const chapterName = step?.chapter || ""
+      return Chapters.getLabel(chapterName)
+    },
+  },
+}
+</script>
+
+<style lang="scss"></style>
