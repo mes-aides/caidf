08ab31c8c017afd01c23967c8adf038032aa0c93
kout95 <antoine.bernier@outlook.fr>
2022-03-23 10:03:12 +0100

    Change le texte des cta

--- src/components/benefit-cta-link.vue
+++ src/components/benefit-cta-link.vue
@@ -20,9 +20,9 @@
 import StatisticsMixin from "@/mixins/statistics"
 
 let typeLabels = {
-  teleservice: "Faire une demande en ligne",
-  form: "Accéder au formulaire papier",
-  instructions: "Accéder aux instructions",
+  teleservice: "Je fais une demande en ligne",
+  form: "J'accède au formulaire papier",
+  instructions: "J'accède aux instructions",
   link: "Plus d'informations <i class='fa fa-external-link' aria-hidden='true' role='presentation'></i>",
 }
 
