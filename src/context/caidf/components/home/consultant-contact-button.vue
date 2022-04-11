762819e9cd4b9615404f77c174838eae8a15f4be
kout95 <antoine.bernier@outlook.fr>
2022-04-11 15:12:20 +0200

    Change le bouton contact

--- src/context/caidf/components/home/consultant-contact-button.vue
+++ src/context/caidf/components/home/consultant-contact-button.vue
@@ -1,5 +1,8 @@
 <template>
-  <a class="caidf-button caidf-outline-white caidf-has-text-uppercase"
+  <a
+    class="caidf-button caidf-outline-white caidf-has-text-uppercase"
+    href="https://www.credit-agricole.fr/ca-paris/particulier/campagnes/prendre-rendez-vous-avec-mon-conseiller.html"
+    target="_blank"
     >Je contacte mon conseiller</a
   >
 </template>
