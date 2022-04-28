a9832ff2386ebde616dd327aa9eff760e942b9fa
kout95 <antoine.bernier@outlook.fr>
2022-04-28 17:42:15 +0200

    Ajoute la dernière section

---
+++ src/context/caidf/components/home/third-section.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="caidf-home-section caidf-third-section">
+    <div class="caidf-home-container">
+      <div class="caidf-max-width-text">
+        <h2 class="caidf-has-text-center caidf-font-open-sans-semi-bold"
+          >Besoin d’aide ? Votre conseiller reste à votre écoute pour vous
+          accompagner</h2
+        >
+        <div class="caidf-third-buttons">
+          <a class="button primary">JE PRENDS RENDEZ-VOUS</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ThirdSection",
+  data: () => {},
+}
+</script>
